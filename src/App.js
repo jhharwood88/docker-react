6c806f29a05099ba3db9e66959c7bec68ebcc20d
Jon Harwood <jon.harwood@ibm.com>
2020-09-24 14:07:00 -0500

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This course barely functions
+          React!
         </p>
         <a
           className="App-link"
