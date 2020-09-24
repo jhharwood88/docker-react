0d675e34e5a786ed1d1cb9525a3049b2e310ebd3
Jon Harwood <jon.harwood@ibm.com>
2020-09-24 14:16:54 -0500

    reverting changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React!
+          This is React!
         </p>
         <a
           className="App-link"
